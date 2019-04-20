63b7f1bb5b044e1cc505c682c3e5f59a1b18f1c1
Roman Alberto Velez Jimenez <49596443+romanAVJ@users.noreply.github.com>
2019-04-19 22:46:56 -0500

    Cambios 1
    Opera bien con el ejemplo que esta en el grupo de wa, pero mi problema es que las vars Nonbasic tienen que ir con signo contrario a lo que son y las slacks en positivo, pero cuando quiero actyalizar la nueva fucion objetivo, los signos afectan el como se suman y no te da el verdadero valor.

--- pivote.m
+++ pivote.m
@@ -1,8 +1,9 @@
-function [outputArg1,outputArg2] = pivote(inputArg1,inputArg2) %no se que se pone
+function [A,b,c] = pivote(A, b, c)
 %Funcion pivote del libro vanderbrei
 %   Dantzing rule
 
 iter = 0;
+eps = -10^-3;
 
 while max(c) > eps
     % pick largest coefficient
@@ -13,40 +14,39 @@ while max(c) > eps
 
     % select leaving variable
     
-    %Case UNBOUNDED
+
     if sum(Acol < -eps) == 0 %counts how many var imposes a restriction
         opt = -1; % unbounded
         disp("The LP is unbounded");
-    else
-    
-        %%%%% QUE ES EPS ??? %%%%%%
+    else 
+        %Not UNBOUNDED
+        nums = b.*(Acol < -eps); %Multiplica por 1 la entrada bi si es cierta la condicion
+        dens = -Acol.*(Acol < -eps);
+        [t, row] = min(nums./dens); %Selects the var which imposes strongest constrain
+        %t <-- the strongest constrain
+        %row <-- index of the strongest constrain
+
+        %%AQUI PUEDE ENTRAR Blands Rule
+
+        Arow = A(row,:);  %Select the row
         
-    %Not UNBOUNDED
-    nums = b.*(Acol < -eps); %Multiplica por 1 la entrada bi si es cierta la condicion
-    dens = -Acol.*(Acol < -eps); %Lo mismo que el pasado
-    [t, row] = min(nums./dens); %Selects the var which imposes strongest constrain
-    %t <-- the strongest constrain
-    %row <-- index of the strongest constrain
-    
-    %%AQUI PUEDE ENTRAR Blands Rule
-    
-    Arow = A(row,:);  %Select the row
-    
-    a = A(row,col) % Pivot element (the var who´s going out)
-    A = A - Acol*Arow/a
-    A(row,:) = -Arow/a
-    A(:,col) = Acol/a %Resuelve por kind of eliminacion gaussiana
-    A(row,col) = 1/a %esto lo podemos mejorar!!!
-    brow = b(row)
-    b = b - brow*Acol/a
-    b(row) = -brow/a
-    ccol = c(col)
-    c = c - ccol*Arow/a
-    c(col) = ccol/a
-    
-    iter = iter+1;
+        %I believe is blands rule
+        a = A(row,col); % Pivot element (the var whoÂ´s going out)
+        A = A - Acol*Arow/a;
+        A(row,:) = -Arow/a; %new tableu for A
+%         A(:,col) = Acol/a; % ARRUINA EL TABLEU
+%         A(row,col) = 1/a; %ARRUINA EL TABLEU
+        brow = b(row);
+        b = b - brow*Acol/a;
+        b(row) = -brow/a;
+        ccol = c(col);
+        c = c - ccol*Arow/a;
+        c(col) = ccol/a;
+
+        iter = iter+1;
     end
 
 
 end
+
 
